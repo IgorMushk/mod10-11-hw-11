64ec21be3208cd33493572b78f235ddbfafd76ce
IgorMushk <igor.mushketik@gmail.com>
2023-08-07 21:40:57 +0300

    Update - then().catch() -> async/await{ try{} catch{}}
    function getData(searchQuery, currentPage, PER_PAGE) {
    fetchImages(searchQuery, currentPage, PER_PAGE)
    .then(data => {
    }).catch(err => console.log(err));
    }
    to
    ----------------
    async function getData(searchQuery, currentPage, PER_PAGE) {
    try {
        const data = await fetchImages(searchQuery, currentPage, PER_PAGE);
     } catch (err) {
        console.log(err);
     }
    }

--- src/03-index.js
+++ src/03-index.js
@@ -68,51 +68,56 @@ function onSubmitSearch(event) {
   getData(searchQuery, currentPage, PER_PAGE);
 }
 
-function getData(searchQuery, currentPage, PER_PAGE) {
-  fetchImages(searchQuery, currentPage, PER_PAGE)
-    .then(data => {
-      console.log(currentPage);
-      if (!data.hits.length & !data.totalHits) {
-        loadMoreRef.classList.add('is-hidden');
-        return Notify.failure(
-          'Sorry, there are no images matching your search query. Please try again.'
-        );
-      }
-      totalHits = data.totalHits;
-      if (currentPage === 1) {
-        Notify.success(`Hooray! We found ${totalHits} images.`);
-      }
+async function getData(searchQuery, currentPage, PER_PAGE) {
+  //fetchImages(searchQuery, currentPage, PER_PAGE)
+  try {
+    const data = await fetchImages(searchQuery, currentPage, PER_PAGE);
+    //-.then(data => {
+    console.log(currentPage);
+    if (!data.hits.length & !data.totalHits) {
+      loadMoreRef.classList.add('is-hidden');
+      return Notify.failure(
+        'Sorry, there are no images matching your search query. Please try again.'
+      );
+    }
+    totalHits = data.totalHits;
+    if (currentPage === 1) {
+      Notify.success(`Hooray! We found ${totalHits} images.`);
+    }
 
-      renderGallery(data.hits);
-      simpleLightBoxLightbox.refresh();
-      observer.observe(target); // + for infiniti scroll
+    renderGallery(data.hits);
+    simpleLightBoxLightbox.refresh();
+    observer.observe(target); // + for infiniti scroll
 
-      //   if (data.hits.length === data.totalHits) {
-      //     // 'zaz'
-      //     loadMoreRef.classList.add('is-hidden');
-      //     Notify.warning(
-      //       "We're sorry, but you've reached the end of search results."
-      //     );
-      //   }
-      if (data.hits.length < data.totalHits) {
-        //loadMoreRef.classList.remove('is-hidden'); //- for Btn  loadMore
-      }
-      // -2 load
-      if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
-        loadMoreRef.classList.add('is-hidden');
-        Notify.warning(
-          "We're sorry, but you've reached the end of search results."
-        );
-        observer.unobserve(target); // +
-      } else {
-        //loadMoreRef.classList.remove('is-hidden'); // -
-      }
-      //   if (currentPage > 1) {
-      //     console.log(currentPage);
-      //     smoothScroll(galleryRef);
-      //   }
-    })
-    .catch(err => console.log(err));
+    //   if (data.hits.length === data.totalHits) {
+    //     // 'zaz'
+    //     loadMoreRef.classList.add('is-hidden');
+    //     Notify.warning(
+    //       "We're sorry, but you've reached the end of search results."
+    //     );
+    //   }
+    if (data.hits.length < data.totalHits) {
+      //loadMoreRef.classList.remove('is-hidden'); //- for Btn  loadMore
+    }
+    // -2 load
+    if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
+      loadMoreRef.classList.add('is-hidden');
+      Notify.warning(
+        "We're sorry, but you've reached the end of search results."
+      );
+      observer.unobserve(target); // +
+    } else {
+      //loadMoreRef.classList.remove('is-hidden'); // -
+    }
+    //   if (currentPage > 1) {
+    //     console.log(currentPage);
+    //     smoothScroll(galleryRef);
+    //   }
+    //-})
+    //-.catch(err => console.log(err));
+  } catch (err) {
+    console.log(err);
+  }
 }
 
 function onLoadInfinitiScroll(entries, observer) {
