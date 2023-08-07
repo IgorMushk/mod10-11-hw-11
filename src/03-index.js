0ed2e5d3f5a18fee48bb355f31c1eb3e4405eafa
IgorMushk <igor.mushketik@gmail.com>
2023-08-07 17:34:30 +0300

    Add getDate()
    fetchImages(searchQuery, currentPage, PER_PAGE) from function onSubmitSearch(event) and function onLoadInfinitiScroll(entries, observer)  move to function getData(searchQuery, currentPage, PER_PAGE)

--- src/03-index.js
+++ src/03-index.js
@@ -64,6 +64,40 @@ function onSubmitSearch(event) {
     return Notify.failure('Enter a search string');
   }
 
+  getData(searchQuery, currentPage, PER_PAGE);
+  // move to getDate()
+  //   fetchImages(searchQuery, currentPage, PER_PAGE)
+  //     .then(data => {
+  //       if (!data.hits.length & !data.totalHits) {
+  //         loadMoreRef.classList.add('is-hidden');
+  //         return Notify.failure(
+  //           'Sorry, there are no images matching your search query. Please try again.'
+  //         );
+  //       }
+  //       totalHits = data.totalHits;
+  //       if (currentPage === 1) {
+  //         Notify.success(`Hooray! We found ${totalHits} images.`);
+  //       }
+
+  //       renderGallery(data.hits);
+  //       simpleLightBoxLightbox.refresh();
+  //       observer.observe(target); // + for infiniti scroll
+
+  //       if (data.hits.length === data.totalHits) {
+  //         // 'zaz'
+  //         loadMoreRef.classList.add('is-hidden');
+  //         Notify.warning(
+  //           "We're sorry, but you've reached the end of search results."
+  //         );
+  //       }
+  //       if (data.hits.length < data.totalHits) {
+  //         //loadMoreRef.classList.remove('is-hidden'); //- for Btn  loadMore
+  //       }
+  //     })
+  //     .catch(err => console.log(err));
+}
+
+function getData(searchQuery, currentPage, PER_PAGE) {
   fetchImages(searchQuery, currentPage, PER_PAGE)
     .then(data => {
       if (!data.hits.length & !data.totalHits) {
@@ -91,6 +125,18 @@ function onSubmitSearch(event) {
       if (data.hits.length < data.totalHits) {
         //loadMoreRef.classList.remove('is-hidden'); //- for Btn  loadMore
       }
+      // -2 load
+      if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
+        loadMoreRef.classList.add('is-hidden');
+        Notify.warning(
+          "We're sorry, but you've reached the end of search results."
+        );
+        observer.unobserve(target); // +
+      } else {
+        //loadMoreRef.classList.remove('is-hidden'); // -
+      }
+      smoothScroll(galleryRef);
+      //
     })
     .catch(err => console.log(err));
 }
@@ -122,23 +168,26 @@ function onLoadInfinitiScroll(entries, observer) {
   entries.forEach(entry => {
     if (entry.isIntersecting) {
       currentPage += 1;
-      fetchImages(searchQuery, currentPage, PER_PAGE)
-        .then(data => {
-          renderGallery(data.hits);
-          simpleLightBoxLightbox.refresh();
 
-          if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
-            loadMoreRef.classList.add('is-hidden');
-            Notify.warning(
-              "We're sorry, but you've reached the end of search results."
-            );
-            observer.unobserve(target); // +
-          } else {
-            //loadMoreRef.classList.remove('is-hidden'); // -
-          }
-          smoothScroll(galleryRef);
-        })
-        .catch(err => console.log(err));
+      getData(searchQuery, currentPage, PER_PAGE);
+      // move to getDate()
+      //   fetchImages(searchQuery, currentPage, PER_PAGE)
+      //     .then(data => {
+      //       renderGallery(data.hits);
+      //       simpleLightBoxLightbox.refresh();
+
+      //       if (currentPage === Math.ceil(data.totalHits / PER_PAGE)) {
+      //         loadMoreRef.classList.add('is-hidden');
+      //         Notify.warning(
+      //           "We're sorry, but you've reached the end of search results."
+      //         );
+      //         observer.unobserve(target); // +
+      //       } else {
+      //         //loadMoreRef.classList.remove('is-hidden'); // -
+      //       }
+      //       smoothScroll(galleryRef);
+      //     })
+      //     .catch(err => console.log(err));
     }
   });
 }
